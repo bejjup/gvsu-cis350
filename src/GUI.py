5d385afa1565c7afcf85657dda5d7afe93fa2c54
Brody Richards <brodyrichards23@gmail.com>
2024-10-08 15:13:15 -0400

    Added Background

--- src/GUI.py
+++ src/GUI.py
@@ -1,16 +1,36 @@
 import pygame as pg
-
+import pygame_gui as GUI
 class GUI:
     def __init__(self) -> None:
         pg.init()
-        # self.__model = MineModel()
-        self._screen = pg.display.set_mode((800, 600))
-        pg.display.set_caption("Minesweeper Roguelike")
-        self._screen.fill((255, 255, 255))
+        self.screen = pg.display.set_mode((1280, 480), pg.SCALED)
+        pg.display.set_caption("Minesweeper")
+
+        # Create The Background
+        self.background = pg.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((170, 155, 187))
         
+    def __run_game__(self):
+        going = True
+        clock = pg.time.Clock()
+        while going:
+            clock.tick(60)
+
+            # Handle Input Events
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    going = False
+                elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                    going = False
+            
+            # Draw to screen
+            self.screen.blit(self.background, (0, 0))
+            pg.display.flip()
+
 def main():
     g = GUI()
-    
+    g.__run_game__()
     
 if __name__ == '__main__':
     main()
