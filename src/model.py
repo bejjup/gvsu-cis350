75d3d1587545ec8f0103e46c8d6e4c7baf62089d
aidenamack <aidenamack@gmail.com>
2024-11-05 20:48:04 -0500

    added string methods to model for testing and updated model to use Tile class

--- src/model.py
+++ src/model.py
@@ -1,7 +1,10 @@
 class Tile:
-    def __init__(self, tile_type):
-        self.tile_type = tile_type
+    def __init__(self, type = 0):
+        self.tile_type = type
         self.status = 0
+
+    def __str__(self):
+        return str(self.tile_type)
 
 class Model:
     def __init__(self, size: int, mines: int):
@@ -14,9 +17,6 @@ class Model:
             - The total number of mines to be placed on the board
         self.board
             - minesweeper board
-        self.revealed_board
-            - the portion of the board revealed to the player
-            - 0s are not revealed and 1s are revealed
 
         """
         self.mines = mines
@@ -25,19 +25,8 @@ class Model:
         for i in range(size):
             row = []
             for j in range(size):
-                row.append(0)
+                row.append(Tile())
             self.board.append(row)
-
-        self.revealed_board = []
-        for i in range(size):
-            row = []
-        for j in range(size):
-            row.append(0)
-        self.revealed_board.append(row)
-
-
-        
-
 
     def generate_numbers(self):
         """
@@ -46,7 +35,7 @@ class Model:
         """
         for row in range(len(self.board)):
             for column in range(len(self.board[row])):
-                if self.board[row][column] == "x":
+                if self.board[row][column].tile_type == -1:
                     self.__generate_numbers_helper(self.board, row, column)
 
     def __generate_numbers_helper(self, board, row, column):
@@ -63,35 +52,45 @@ class Model:
             - the column coordinate of current bomb
         
         """
-
+        print("going to helper")
         # Increments spaces above the mine
         if row - 1 >= 0:
-            if isinstance(board[row - 1][column], int) is True:
-                board[row - 1][column] += 1
+            if board[row - 1][column].tile_type >= 0:
+                board[row - 1][column].tile_type += 1
                 if column - 1 >= 0:
-                    if isinstance(board[row - 1][column - 1], int) is True:
-                        board[row - 1][column - 1] += 1
+                    if board[row - 1][column - 1].tile_type >= 0:
+                        board[row - 1][column - 1].tile_type += 1
                 if column + 1 < len(board[row]):
-                    if isinstance(board[row - 1][column + 1], int) is True:
-                        board[row - 1][column + 1] += 1
+                    if board[row - 1][column + 1].tile_type >= 0:
+                        board[row - 1][column + 1].tile_type += 1
 
         # Increments spaces below the mine
         if row + 1 < len(board):
-            if isinstance(board[row + 1][column], int) is True:
-                self.board[row + 1][column] += 1
+            if board[row + 1][column].tile_type >= 0:
+                self.board[row + 1][column].tile_type += 1
                 if column - 1 >= 0:
-                    if isinstance(board[row + 1][column - 1], int) is True:
-                        board[row + 1][column - 1] += 1
+                    if board[row + 1][column - 1].tile_type >= 0:
+                        board[row + 1][column - 1].tile_type += 1
                 if column + 1 < len(board[row]):
-                    if isinstance(board[row + 1][column + 1], int) is True:
-                        board[row + 1][column + 1] += 1
+                    if board[row + 1][column + 1].tile_type >= 0:
+                        board[row + 1][column + 1].tile_type += 1
 
         # Increments the space to the left of the mine
         if column - 1 >= 0:
-            if isinstance(board[row][column - 1], int) is True:
-                board[row][column - 1] += 1
+            if board[row][column - 1].tile_type >= 0:
+                board[row][column - 1].tile_type += 1
 
         # Increments the space to the right of the mine
         if column + 1 < len(board[row]):
-            if isinstance(board[row][column + 1], int) is True:
-                board[row][column + 1] += 1
+            if board[row][column + 1].tile_type >= 0:
+                board[row][column + 1].tile_type += 1
+
+    def __str__(self):
+        printmodel = []
+        for row in range(len(self.board)):
+            row_placeholder = []
+            for column in range(len(self.board[row])):
+                row_placeholder.append(int(str(self.board[row][column])))
+            printmodel.append(row_placeholder)
+        return str(printmodel)
+            
