5c95d0b4920d8dcad21ac1466ad6b5985fec24b6
DuckuuSensei <dman.toomey@gmail.com>
2024-11-20 10:39:16 -0500

    updated readability

--- src/exampleDATABASEinput.js
+++ src/exampleDATABASEinput.js
@@ -1,6 +1,20 @@
-import { addItem, getItems } from './src/db';
+import { initializeDatabase, addItem, getItems, updateItem, deleteItem } from './src/db';
 
-async function saveNewItem() {
-  await addItem({ id: 2, name: 'New Item' });
-  console.log(getItems());
+async function exampleUsage() {
+  // Initialize the database
+  await initializeDatabase();
+
+  // Add an item
+  await addItem({ id: 1, name: 'Sample Item' });
+
+  // Get all items
+  const items = getItems();
+  console.log('Items:', items);
+
+  // Update an item
+  await updateItem(1, { name: 'Updated Sample Item' });
+
+  // Delete an item
+  await deleteItem(1);
 }
+exampleUsage();
